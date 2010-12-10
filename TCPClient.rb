957c8b7dfed87248996012e3d06b40c913f19f7c
Bryan Tran <btran@multitech.com>
2010-12-09 22:43:39 -0600

    Delete the comment

--- TCPClient.rb
+++ TCPClient.rb
@@ -1,7 +1,5 @@
 require 'net/telnet'
 
-#This is a new comment added - Do the commit with out 
-#add and expect it will not add this comment
 server = Net::Telnet::new('Host' => '127.0.0.1',
 'Port' => 1234,
 'Telnetmode' => false)
